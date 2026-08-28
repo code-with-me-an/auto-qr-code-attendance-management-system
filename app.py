39cccb485bedb0dcb416356e4ca52527624f4a83
adithyan <nadithyan837@gmail.com>
2026-08-29 01:04:58 +0530

    student dashboard is completed

---
+++ app.py
@@ -0,0 +1,144 @@
+from flask import Flask,render_template,request,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, date, time
+from sqlalchemy.engine import URL
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
+
+app.config["SQLALCHEMY_DATABASE_URI"] = URL.create(
+    "mysql+pymysql",
+    username="root",
+    password=os.getenv('DB_password'),
+    host="localhost",
+    port=3306,
+    database="aakrithi_attendance"
+)
+app.config["SQLALCHEMY_TRACk_MODIFICATIONS"] = False
+# app.config["SQLALCHEMY_ECHO"] = True 
+
+db = SQLAlchemy(app)
+
+
+class Student(db.Model):
+    __tablename__ = "students"
+
+    student_id = db.Column(db.String(20), primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    roll_number = db.Column(db.String(50), nullable=False)
+    department = db.Column(db.String(50), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(120), unique=True)
+    phone = db.Column(db.String(20), nullable=False)
+
+    attendances = db.relationship("Attendance", back_populates="student")
+
+
+class Event(db.Model):
+    __tablename__ = "events"
+    event_id = db.Column(db.Integer, primary_key=True)
+    event_name = db.Column(db.String(100), nullable=False)
+    event_date = db.Column(db.Date, nullable=False)
+    start_time = db.Column(db.Time)
+    end_time = db.Column(db.Time)
+
+    attendances = db.relationship("Attendance", back_populates="event")
+
+
+class Attendance(db.Model):
+    __tablename__ = "attendance"
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.String(20), db.ForeignKey("students.student_id"), nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey("events.event_id"), nullable=False)
+    marked_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
+
+    student = db.relationship("Student", back_populates="attendances")
+    event = db.relationship("Event", back_populates="attendances")
+
+    __table_args__ = (
+        db.UniqueConstraint("student_id", "event_id", name="unique_student_event"),
+    )
+
+with app.app_context():
+        db.create_all()
+        # new_student = Attendance(
+        #         student_id = 'AAK030',
+        #         event_id = 1,
+        #     )
+        # db.session.add(new_student)
+        # db.session.commit()
+        # print(Student.query.all())
+
+
+@app.route('/')
+def dashboard():
+    total_students = Student.query.count()
+    total_events = Event.query.count()
+    total_attendance = Attendance.query.count()
+
+    today = date.today()
+
+    today_attendance = Attendance.query.filter(
+         db.func.date(Attendance.marked_at) == today
+    ).all()
+
+    for attendance in today_attendance:
+         print(attendance.student.name)
+
+    event_statistics = []
+    events = Event.query.order_by(
+         Event.event_date.desc()
+    ).all()
+
+    for event in events:
+        count = Attendance.query.filter_by(
+            event_id = event.event_id
+        ).count()
+
+        percentage = 0
+
+        if total_students > 0: 
+            percentage = round( 
+                (count/total_students)*100,
+                1
+            )
+        event_statistics.append(
+             {
+                  "event":event,
+                  "count":count,
+                  "percentage":percentage
+             }
+        )
+
+    return render_template(
+         "dashboard.html",
+         total_students=total_students,
+         total_events=total_events,
+         total_attendance=total_attendance,
+         today_attendance=today_attendance,
+         event_statistics=event_statistics
+    )
+@app.route('/system_data')
+def system_data():
+     return render_template(
+          "system_data.html"
+     )
+
+@app.route('/attendance')
+def attendance():
+     return render_template(
+          "attendance.html"
+     )
+
+
+
+    
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
